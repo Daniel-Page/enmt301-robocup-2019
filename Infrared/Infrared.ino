38354377fa9bca7348083677fabedd13f2f7d18d
Daniel-Page <dwpage98@gmail.com>
2019-04-23 17:49:25 +1200

    IR Sensor

---
+++ Infrared/Infrared.ino
@@ -0,0 +1,14 @@
+int analogInPin = A0;  
+int sensorValue = 0;
+
+
+void setup() {
+  Serial.begin(9600); // 9600 bps
+}
+
+
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  Serial.println(sensorValue);
+  delay(2); // 2 ms ADC settling time
+}
