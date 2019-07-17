095dc20fe673c2cbfd2e5b41972c35a1f2883015
Daniel-Page <dwpage98@gmail.com>
2019-07-17 17:55:06 +1200

    Both IR sensors blocked

--- main/main.ino
+++ main/main.ino
@@ -6,17 +6,24 @@
 
 #include <avr/wdt.h>
 #include "motors.h"
-//#include "sensors.h"
 
 
 #define BAUD_RATE 9600 // Bits/s
 
+
 // Definitions for IR sensor
 int rightSensor = A0; 
 int rightValue = 0;
+
 int leftSensor = A2; 
 int leftValue = 0;
-int blocked = (rightValue < 200 && leftValue < 200);
+
+int left_blocked = 0;
+int right_blocked = 0;
+int blocked_left_count = 0;
+int blocked_right_count = 0;
+
+
 
 enum modes {SEARCHING, KNOCKOVER, PICKUP, BACKOFF, FINISHED}; // Implement states later
 
@@ -26,16 +33,11 @@ void setup()
     pinMode(49, OUTPUT); // Pin 49 is used to enable IO power
     digitalWrite(49, 1); // Enable IO power on main CPU board
 
-    //wdt_enable(WDTO_2S); // Watchdog timer set to trigger after 2 seconds if not reset
+    wdt_enable(WDTO_2S); // Watchdog timer set to trigger after 2 seconds if not reset
 
-    // Initialise serial input and buffer
     Serial.begin(BAUD_RATE); // Initialises serial
     
     initMotors();
-    // setMotor(RIGHT, CLOCKWISE, 50);   // Sets the right motor to 100% speed clockwise
-    // setMotor(LEFT, CLOCKWISE, 50);    // Sets the left motor to 100% speed anticlockwise
-    // turnRobot(CLOCKWISE, 50);
-    // setMotor(LEFT, STATIONARY, 0); // Stops left motor
 }
 
 
@@ -44,19 +46,29 @@ void loop()
   rightValue = analogRead(rightSensor);
   Serial.println(rightValue);
   leftValue = analogRead(leftSensor);
-  Serial.println(rightValue);
-  delay(1);        // delay in between reads for stability
+  Serial.println(leftValue);
+  delay(1); // delay in ms between reads for stability
 
-  if (rightValue < 200 && leftValue < 200) {
-    blocked = 0;
-    setMotor(RIGHT, CLOCKWISE, 75);   // Sets the right motor to 100% speed clockwise
-    setMotor(LEFT, CLOCKWISE, 75); 
-  } else if (blocked) {
-      turnRobot(ANTICLOCKWISE, 50);
-  } else if (rightValue > 200 && !blocked) {
-    turnRobot(ANTICLOCKWISE, 50);
-  } else if (leftValue > 200 && !blocked) {
-    turnRobot(CLOCKWISE, 50);
+
+  if (rightValue < 200 && leftValue < 200) { 
+      setMotor(RIGHT, CLOCKWISE, 75);  
+      setMotor(LEFT, CLOCKWISE, 75);
+  } else if (rightValue >= 200 && leftValue >= 200) {
+        blocked = 1;
+        turnRobot(ANTICLOCKWISE, 50); // Runs when both sensors are block for 100 counts
+  } else if (rightValue >= 200 && !blocked) {
+        turnRobot(ANTICLOCKWISE, 50);
+  } else if (leftValue >= 200 && !blocked) {
+        turnRobot(CLOCKWISE, 50);
   }
-    //wdt_reset(); // Resets watchdog timer
+
+
+  if (block_count >= 100) {
+      blocked = 0;
+  } else {
+      blocked_count++;
+  }
+
+    
+    wdt_reset(); // Resets watchdog timer
 }
